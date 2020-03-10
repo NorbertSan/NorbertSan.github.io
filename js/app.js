f6867744be15758e6966658ca4725f09499dc5a4
NorbertSan <norbertsanpruch@gmail.com>
2020-03-10 23:20:45 +0100

    Added keyboards moves and touchmoves to slider

--- js/app.js
+++ js/app.js
@@ -28,6 +28,11 @@ const slider = () => {
   let sliderWidth = slider.getBoundingClientRect().width;
   const dots = document.querySelectorAll(".projectsSection__dots__singleDot");
   const images = slider.querySelectorAll("img");
+  const liveLink = document.querySelector(".liveLink");
+  const codeLink = document.querySelector(".codeLink");
+  const buttons = document.querySelectorAll(
+    ".projectsSection__frame__projectInfo__buttons"
+  );
   const name = document.querySelector(
     ".projectsSection__frame__projectInfo__name"
   );
@@ -38,17 +43,23 @@ const slider = () => {
     {
       name: "Chess",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit, modi quas rem facilis vitae adipisci aliquid quo nemo?"
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit, modi quas rem facilis vitae adipisci aliquid quo nemo? rem facilis vitae adipisci aliquid quo nemo? rem facilis vitae adipisci aliquid quo nemo? rem facilis vitae adipisci aliquid quo nemo? rem facilis vitae adipisci aliquid quo nemo?",
+      liveLink: "http://google.com",
+      codeLink: "https://www.youtube.com/"
     },
     {
       name: "Hamburger landing page",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit, modi quas rem facilis vitae sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis"
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit, modi quas rem facilis vitae sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis",
+      liveLink: "http://google.com",
+      codeLink: "https://www.youtube.com/"
     },
     {
       name: "Snake",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit"
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor odit corrupti consectetur ipsam praesentium Nobis reprehenderit",
+      liveLink: "http://google.com",
+      codeLink: "https://www.youtube.com/"
     }
   ];
   let index = 1;
@@ -59,15 +70,37 @@ const slider = () => {
     dots[1].classList.add("active");
     name.textContent = projectsInfo[1].name;
     description.textContent = projectsInfo[1].description;
-  };
-  init();
+    // codeLink;
+    leftArrow.addEventListener("click", slideLeft);
+    rightArrow.addEventListener("click", slideRight);
 
+    document.body.addEventListener("keyup", e => {
+      e.keyCode === 37 && slideLeft();
+      e.keyCode === 39 && slideRight();
+    });
+
+    let lastPosition;
+    let currentPosition;
+    slider.addEventListener("touchstart", e => {
+      lastPosition = e.touches[0].clientX;
+    });
+    slider.addEventListener("touchend", e => {
+      currentPosition = e.changedTouches[0].clientX;
+      if (Math.abs(currentPosition - lastPosition) < 25) return;
+      if (currentPosition > lastPosition) {
+        slideLeft();
+      } else if (currentPosition < lastPosition) {
+        slideRight();
+      }
+    });
+  };
   const disableClick = () => {
     leftArrow.style.pointerEvents = "none";
     rightArrow.style.pointerEvents = "none";
     setTimeout(() => {
       leftArrow.style.pointerEvents = "all";
       rightArrow.style.pointerEvents = "all";
+      document.body.style.pointerEvents = "all";
     }, 600);
   };
   const setActiveDot = () => {
@@ -78,17 +111,30 @@ const slider = () => {
   };
   const changeProjectInfo = () => {
     const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
-    tl.to([name, description], { duration: 0.3, y: "30", opacity: 0 })
+    tl.to([name, description, ...buttons], {
+      duration: 0.3,
+      y: "20",
+      opacity: 0
+    })
       .set(name, { textContent: projectsInfo[index % dots.length].name })
       .set(description, {
         textContent: projectsInfo[index % dots.length].description
       })
-      .to([name, description], { duration: 0.3, y: "0", opacity: 1 });
+      .set(codeLink, {
+        href: projectsInfo[index % dots.length].codeLink
+      })
+      .set(liveLink, {
+        href: projectsInfo[index % dots.length].liveLink
+      })
+      .to([name, description, ...buttons], {
+        duration: 0.3,
+        y: "0",
+        opacity: 1
+      });
 
     // name.textContent = projectsInfo[index % dots.length].name;
     // description.textContent = projectsInfo[index % dots.length].description;
   };
-
   const slideLeft = () => {
     disableClick();
     setTimeout(() => (sliderWidth = slider.getBoundingClientRect().width), 0);
@@ -103,9 +149,12 @@ const slider = () => {
     index++;
     slider.style.transform = `translateX(-${sliderWidth * index}px)`;
   };
-
   slider.addEventListener("transitionend", () => {
     setTimeout(() => (sliderWidth = slider.getBoundingClientRect().width), 0);
+    // prevent multiclick keyboard arrows
+    if (index > amountOfImages - 1) index = 0;
+    if (index < 0) index = amountOfImages - 1;
+
     if (images[index].id === "lastClone") {
       slider.style.transition = "none";
       index = 1;
@@ -119,8 +168,8 @@ const slider = () => {
     setActiveDot();
     changeProjectInfo();
   });
-  leftArrow.addEventListener("click", slideLeft);
-  rightArrow.addEventListener("click", slideRight);
+
+  init();
 };
 
 const init = () => {
